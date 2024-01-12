bdd349e6f01beca42dd2971f9ed22ceb3cbfd45a
pdrVenancio <pedrosantos121229@gmail.com>
2024-01-12 17:21:05 -0300

    ForEach

---
+++ 8Secao-jsArray/forEach.js
@@ -0,0 +1,26 @@
+const apr = ['pedro', 'celenia', 'joao', 'jamili', 'joaqim']
+
+//                  (valor, indice, array)
+apr.forEach(function(nome, indice){
+    console.log(`${indice + 1}) ${nome}`)
+})
+
+apr.forEach(nome => console.log(nome))
+
+const exibirAprovados = apr => console.log(apr)
+apr.forEach(exibirAprovados)
+
+// Aula 2
+console.log("\n\nAula 2\n")
+
+Array.prototype.forEach2 = function (callback){
+    for(let i = 0; i < this.length; i++)
+        callback(this[i], i, this)
+}
+
+apr.forEach2(function(nome, indice){
+    console.log(`${indice + 1}) ${nome}`)   
+})
+
+// Aula 2
+console.log("\n\nAula 2\n")
