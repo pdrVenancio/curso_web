3074503569881c91fd8e4d75886f7f358649ce06
pdrVenancio <pedrosantos121229@gmail.com>
2024-03-25 22:14:47 -0300

    Formulario

--- 11Secao-HTML/Extruturando_exercicios/exercicos/form/server.js
+++ 11Secao-HTML/Extruturando_exercicios/exercicos/form/server.js
@@ -7,7 +7,12 @@ app.use(bodyParser.urlencoded({extended: true}))
 
 app.post('/usuarios', (req, resp) => {
     console.log(req.body)
-    resp.send('<h1> Parabens </h1>')
+    resp.send('<h1> Parabens! Usuario cadastrado </h1>')
+})
+
+app.post('/usuarios/:id', (req, resp) => {
+    console.log(req.body)
+    resp.send('<h1> Parabens! Usuario Alterado! </h1>')
 })
 
 app.listen(5500)
