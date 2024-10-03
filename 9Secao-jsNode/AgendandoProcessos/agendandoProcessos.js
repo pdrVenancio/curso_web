6ec133967428e04dd804b02a4fc61e0c8142edac
Pedro Venâncio dos Santos <89045459+pdrVenancio@users.noreply.github.com>
2024-10-03 11:17:47 -0300

    Update agendandoProcessos.js

--- 9Secao-jsNode/AgendandoProcessos/agendandoProcessos.js
+++ 9Secao-jsNode/AgendandoProcessos/agendandoProcessos.js
@@ -1,74 +1,22 @@
-async function consultaResponsaveisProduto() {
+// npm i node-schedule
 
-    let request = await fetchData(2128, `${null}`)
-    let response = await request
-  
-    const SELECT_responsavel = document.querySelector('#consulta-responsaveis-ce-mult') 
-    
-    response.map(e =>{
-        if(e.Grupo == 'SSCR_ENGENHARIA_PRODUTO'){
-            const option = document.createElement('option')
-    
-            option.innerText = e.FUNCIONARIO
-            option.value = e.Matricula
-    
-            SELECT_responsavel.add(option)
-        }
-    })
+const schedule = require('node-schedule')
 
-    return 
-}
+// ( quanto en quanto tempo | qual minuto | qual hora | dia do mes | mes | dia da semana)
+const tarefa1 = schedule.scheduleJob('*/5 * 12 * * 2', function(){
+    console.log('Executando Tarefa 1', new Date().getSeconds())
+})
 
-async function casoCE(){
-    $('#modal_responsaveis_ce').modal('show');
-    await consultaResponsaveisProduto()
-    
-    const BTN_salvar = document.querySelector('#btn-salvar-responsaveis-ce')
+setTimeout(function(){
+    tarefa1.cancel()
+    console.log('Cancelando tarefa 1')
+}, 20000)
 
-    return BTN_salvar.addEventListener('click', () => {
-        const lista_responsaveis_ce = $(`#responsavei_email_ce`).val();
-        const responsaveis_ce = lista_responsaveis_ce.join(',').toString();
-    
-        if(responsaveis_ce == '' || responsaveis_ce == undefined){
-            return [false, '']
-        }
-    
-        return [true, responsaveis_ce]
-    })
-}
+const regra = new schedule.RecurrenceRule()
+regra.dayOfWeek = [new schedule.Range(1, 5)]
+regra.hour = 12
+regra.second = 30
 
-// AltPV 31 - Limitar acesso ao botao Tela PM
-async function salvarAnotacao(){
-    let verifica_acesso = await verificaAcessoPMO()
-
-    let status = $(`#status-global`).val();
-    let prioridade = $(`#prioridade-sscr`).val();
-    let anotacao = $(`#anotacao-novo`).val();
-    let tipo_processo = $(`#tipo`).val()
-    let recorrencia
-    let responsaveis_ce
-    const tipoAnotação = 2;
-
-    if(anotacao == undefined){
-        anotacao = '';
-    }
-    if(status == 5){
-        recorrencia = await casoRecorrencia()
-        if(recorrencia[0] == false){
-            return
-        }
-    }  
-
-    // verifica CE
-    if(tipo_processo == 1 && status == 2){
-        console.log('opa')
-        
-        responsaveis_ce = await casoCE()
-        console.log(responsaveis_ce)
-        if(responsaveis_ce[0] == false){
-            return
-        }
-    }
-
-    console.log('deu merda: ', responsaveis_ce)
-}
+const tarefa2 = schedule.scheduleJob(regra, function(){
+    console.log('Exec tarefa 2', new Date().getSeconds())
+})
